b6b622dd9225557abeb2f265f884c68b18588d08
JanoVGC <altisentbrescojan@alumnes.ilerna.com>
2024-12-02 16:42:37 +0100

    fix

---
+++ src/matrius_debugging/Matrius_debugging.java
@@ -0,0 +1,10 @@
+package matrius_debugging;
+
+public class Matrius_debugging {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author alumnat
+ *
+ */
+module matrius_debugging {
+}
